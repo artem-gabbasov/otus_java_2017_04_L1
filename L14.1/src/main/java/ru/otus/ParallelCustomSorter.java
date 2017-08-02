b0a87c714c03aa5af9f7f10fdeb6173add194055
artem-gabbasov <artem.gabbasov@gmail.com>
2017-08-02 05:08:59 +0500

    Параллельные сортировки переделаны под SortingTask

--- L14.1/src/main/java/ru/otus/ParallelCustomSorter.java
+++ L14.1/src/main/java/ru/otus/ParallelCustomSorter.java
@@ -1,34 +1,52 @@
 package ru.otus;
 
-import sun.rmi.runtime.Log;
-
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
 import java.util.logging.Logger;
 
 /**
  * Created by Artem Gabbasov on 02.08.2017.
  * Абстрактный класс параллельного сортировщика
  */
-public abstract class ParallelCustomSorter<T extends Comparable<T>> extends SerialCustomSorter<T> {
+public abstract class ParallelCustomSorter<T extends Comparable<T>> extends SimpleCustomSorter<T> {
     /**
      * Пороговое значение количества элементов, меньше которого не имеет смысла делить на потоки (всё выполняется в едином потоке)
      */
     final static int PARALLEL_THRESHOLD = 4;
 
     @Override
-    protected void sortPart(SortingArguments<T> args, Consumer<SortingArguments<T>> sortingFunction) {
-        if (isSerialPreferable(args)) {
-            super.sortPart(args, sortingFunction);
-        } else {
-            Logger.getLogger(LOGGER_NAME).info("p");
-            sortPartParallel(args, sortingFunction);
+    protected void sortParts(SortingTask<T>.TasksPair pair) {
+        Thread threadLeft = startThread(pair.getLeft());
+        Thread threadRight = startThread(pair.getRight());
+
+        try {
+            if (threadLeft != null) {
+                threadLeft.join();
+            } else {
+                pair.getLeft().perform();
+            }
+
+            if (threadRight != null) {
+                threadRight.join();
+            } else {
+                pair.getRight().perform();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
-    protected boolean isSerialPreferable(SortingArguments<T> args) {
-        return args.getArray().length <= PARALLEL_THRESHOLD;
+    protected Thread startThread(SortingTask<T> part) {
+        if (isSerialPreferable(part)) {
+            Logger.getLogger(LOGGER_NAME).info("s");
+            return null;
+        } else {
+            Logger.getLogger(LOGGER_NAME).info("p");
+            return sortPartParallel(part);
+        }
     }
 
-    protected abstract void sortPartParallel(SortingArguments<T> args, Consumer<SortingArguments<T>> sortingFunction);
+    protected boolean isSerialPreferable(SortingTask<T> part) {
+        return part.getArray().length <= PARALLEL_THRESHOLD;
+    }
+
+    protected abstract Thread sortPartParallel(SortingTask<T> part);
 }
